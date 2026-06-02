ae12d358824486062c025df3fb698dbc670586da
sulis <sulistyawati513@gmail.com>
2026-06-02 10:56:20 +0700

    yess

---
+++ blabla/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blabla
+{
+    internal class user
+    {
+        user()
+        {
+            Console.WriteLine("User created");
+        }
+    }
+}
