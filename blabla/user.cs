de26ebbaabcd03f48626c5307dfdb483c6718428
Akbar Udin <hailakbar2007@gmail.com>
2026-06-02 10:44:00 +0700

    test akbar

--- blabla/user.cs
+++ blabla/user.cs
@@ -6,5 +6,9 @@ namespace blabla
 {
     internal class user
     {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
     }
 }
